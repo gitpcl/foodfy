72d4bb41cc481c8e044787cefcb1d1f6b4482c83
Pedro Lopes <admin@pedroclopes.com>
2021-02-10 17:05:11 -0600

    Added feature to update modal info when clicked on recipe page

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,11 +4,22 @@ const closeModal = document.querySelector('.close-modal')
 
 for (let card of cards) {
     card.addEventListener("click", function() {
+        const videoId = card.getAttribute('id')
+        const modalTitle = card.querySelector('h2').innerHTML
+        const modalChef = card.querySelector('p').innerHTML
+
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector('img').src = `./assets/${videoId}.png`
+        modalOverlay.querySelector('h2').innerHTML = modalTitle
+        modalOverlay.querySelector('p').innerHTML = modalChef
+        
     })
 }
 
 
 closeModal.addEventListener('click', function() {
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('img').src = ""
+    modalOverlay.querySelector('h2').innerHTML = ""
+    modalOverlay.querySelector('p').innerHTML = ""
 })
