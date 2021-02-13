003c1b7488d2b5b0431e6b528a6dddaf33f997cd
Pedro Lopes <admin@pedroclopes.com>
2021-02-12 21:50:01 -0600

    Added browser-sync and run-all

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const recipes = require('./data')
 const routes = express.Router()
 
+// Public routes
 routes.get("/", (req, res) => {
     return res.render('landing', { items: recipes })
 })
@@ -31,6 +32,17 @@ routes.get("/recipes/:index", (req, res) => {
     
 })
 
+// Admin routes
+// routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
+// routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+// routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+// routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
+
+// routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+// routes.put("/admin/recipes", recipes.put); // Editar uma receita
+// routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
+
+// 404 route
 routes.use((req, res) => {
     res.status(404).render("not-found");
 });
